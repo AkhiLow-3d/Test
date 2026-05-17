a1b69cd01472491c808bbea701d1a8b362f7692f
AkhiLow-3d <tatsutaagemono@gmail.com>
2026-05-17 14:05:14 +0900

    Create products.js

---
+++ products.js
@@ -0,0 +1,19 @@
+const products = [
+  {
+    title: "教室パック",
+    description: "学校・教室向けの3D空間素材",
+    image: "images/classroom.jpg",
+    imageAlt: "教室パックのスクリーンショット",
+    boothUrl: "#",
+    vrchatUrl: ""
+  },
+
+  {
+    title: "夜更けの喫茶店 - Cozy Cafe -",
+    description: "夜の喫茶店ワールド素材",
+    image: "images/cozy-cafe.jpg",
+    imageAlt: "Cozy Cafeのスクリーンショット",
+    boothUrl: "https://akhilow.booth.pm/items/7587358",
+    vrchatUrl: "https://vrch.at/zqar9sdk"
+  }
+];
